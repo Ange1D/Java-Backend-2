6e5f0a526833c31b27500265a1fd36dc4cb25d8a
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-05-16 20:54:52 -0600

    S1E1

--- e1/starter/src/main/java/org/bedu/starter/StarterApplication.java
+++ e1/starter/src/main/java/org/bedu/starter/StarterApplication.java
@@ -1,13 +1,56 @@
 package org.bedu.starter;
 
+import org.bedu.starter.utils.Calculadora;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class StarterApplication {
+public class StarterApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StarterApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception{
+		System.out.println("Hello world");
+
+		Scanner sc = new Scanner(System.in);
+		Calculadora c = new Calculadora();
+
+		System.out.println("Calculadora");
+		System.out.println("1. Sumar");
+		System.out.println("1. Restar");
+		System.out.println("1. Multiplicar");
+		int opcion = sc.nextInt();
+
+		if (opcion >3 || opcion <1){
+			System.out.println("Opcion invalida");
+			return;
+		}
+
+		System.out.println("Ingresa el primer numero");
+		int a = sc.nextInt();
+		System.out.println("Ingresa el segundo numero");
+		int b = sc.nextInt();
+
+		int respuesta = 0;
+
+		switch (opcion){
+			case 1:
+				respuesta = c.sumar(a,b);
+				break;
+			case 2:
+				respuesta = c.restar(a,b);
+				break;
+			case 3:
+				respuesta = c.multiplicar(a,b);
+				break;
+		}
+
+		System.out.println("La respuesta es "+ respuesta);
+	}
 }
---
+++ e1/starter/src/main/java/org/bedu/starter/utils/Calculadora.java
@@ -0,0 +1,16 @@
+package org.bedu.starter.utils;
+
+public class Calculadora {
+
+    public int sumar(int a,int b){
+        return a + b;
+    }
+
+    public int restar(int a,int b){
+        return a - b;
+    }
+
+    public int multiplicar(int a,int b){
+        return a * b;
+    }
+}
