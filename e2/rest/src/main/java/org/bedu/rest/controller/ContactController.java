b17def8124be8692a4209cd6d02676b3ffe19873
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-05-23 20:43:45 -0600

    S3E1

--- e2/rest/src/main/java/org/bedu/rest/controller/ContactController.java
+++ e2/rest/src/main/java/org/bedu/rest/controller/ContactController.java
@@ -2,75 +2,54 @@ package org.bedu.rest.controller;
 
 import org.bedu.rest.model.Contact;
 import org.bedu.rest.model.UpdatedContact;
+import org.bedu.rest.service.AgendaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("contacts")
 public class ContactController {
 
-    private Map<String, Contact> agenda;
+    private AgendaService agenda;
 
-    public ContactController(){
-        agenda = new HashMap<String, Contact>();
-
-        agenda.put("angel", new Contact("Angel","000000001"));
+    @Autowired
+    public ContactController(AgendaService agenda){
+        this.agenda = agenda;
     }
 
     //GET /contacts -> Obtener todos los contactos
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Contact> getContacts(){
-        return new LinkedList<Contact>(agenda.values());
+        return agenda.getAll();
     }
 
     //GET /contacts/nombre -> Obtener un contacto
     @GetMapping("{name}")
-    public ResponseEntity<Contact> getContactByName(@PathVariable("name") String name){
-        if(!agenda.containsKey(name)){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(agenda.get(name.toLowerCase()));
+    public Contact getContactByName(@PathVariable("name") String name) {
+        return agenda.getOne(name);
     }
 
     //POST /conctacts -> Crear un contacto
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Contact createContact(@RequestBody Contact newContact) {
-        agenda.put(newContact.getName().toLowerCase(), newContact);
-        return newContact;
+    public Contact createContact(@RequestBody Contact contact) {
+        return agenda.add(contact);
     }
 
     //PUT /conctacts/nombre -> Actualizar un contacto
     @PutMapping("{name}")
-    public ResponseEntity<Void> updateContacto(@RequestBody UpdatedContact contact, @PathVariable("name") String name){
-        if(!agenda.containsKey("name")){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-
-        Contact current = agenda.get(name);
-
-        current.setNumber(contact.getNumber());
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    public void updateContacto(@RequestBody UpdatedContact contact, @PathVariable("name") String name){
+        agenda.update(name, contact);
     }
 
     //DELETE /conctacts/nombre -> Eliminar un contacto
     @DeleteMapping("{name}")
-    public ResponseEntity<Void> deleteContacto(@PathVariable("name") String name){
-        if(!agenda.containsKey("name")){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-
+    public void deleteContacto(@PathVariable("name") String name){
         agenda.remove(name);
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 
 }
---
+++ e2/rest/src/main/java/org/bedu/rest/service/AgendaService.java
@@ -0,0 +1,48 @@
+package org.bedu.rest.service;
+
+import org.bedu.rest.model.Contact;
+import org.bedu.rest.model.UpdatedContact;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class AgendaService {
+    private Map<String, Contact> agenda;
+    public AgendaService(){
+        agenda = new HashMap<String, Contact>();
+        agenda.put("angel", new Contact("Angel","000000001"));
+    }
+
+    public boolean exists(String name){
+        return agenda.containsKey(name);
+    }
+
+
+    public List<Contact> getAll() {
+        return new LinkedList<Contact>(agenda.values());
+    }
+
+    public Contact getOne(String name) {
+        return agenda.get(name.toLowerCase());
+    }
+
+    public Contact add(Contact contact){
+        agenda.put(contact.getName().toLowerCase(), contact);
+        return contact;
+    }
+
+    public void update(String name, UpdatedContact contact){
+        Contact current = agenda.get(name);
+
+        current.setNumber(contact.getNumber());
+
+    }
+
+    public void remove(String name){
+        agenda.remove(name);
+    }
+}
