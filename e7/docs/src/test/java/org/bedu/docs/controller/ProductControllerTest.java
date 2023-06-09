4043b98d361f7265c8753d25263fb43c4263d9ca
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-06-08 22:02:22 -0600

    S8E2

--- e7/docs/src/test/java/org/bedu/docs/controller/ProductControllerTest.java
+++ e7/docs/src/test/java/org/bedu/docs/controller/ProductControllerTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.payload.PayloadDocumentation;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -17,6 +19,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 class ProductControllerTest {
 
     //Permite simular las peticiones HTTP
@@ -31,7 +34,23 @@ class ProductControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(
                         "[{\"code\":\"PR001\",\"name\":\"Gansito\",\"price\":15.0,\"description\":\"Pastelito de chocolate\"},{\"code\":\"PR002\",\"name\":\"Topochico\",\"price\":19.0,\"description\":\"Agua Mineral de Manantial\"},{\"code\":\"PR003\",\"name\":\"Cheetos\",\"price\":10.0,\"description\":\"Fritura de maiz\"}]"
-                ));
+                ))
+                .andDo(MockMvcRestDocumentation.document("products/findAll",
+                        PayloadDocumentation
+                                .responseFields(
+                                        PayloadDocumentation
+                                                .fieldWithPath("[].code")
+                                                .description("El identificador del producto"),
+                                        PayloadDocumentation
+                                                .fieldWithPath("[].name")
+                                                .description("El nombre del producto"),
+                                        PayloadDocumentation
+                                                .fieldWithPath("[].description")
+                                                .description("La descripcion del producto"),
+                                        PayloadDocumentation
+                                                .fieldWithPath("[].price")
+                                                .description("El precio del producto")
+                                )));
     }
 
     @Test
@@ -44,8 +63,24 @@ class ProductControllerTest {
                         .content(
                                 "{\"code\":\"PR004\",\"name\":\"Agua\",\"price\":10.0,\"description\":\"Agua de 500ml\"}"
                         ))
-                .andExpect(MockMvcResultMatchers.status().isCreated());
-        
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andDo(MockMvcRestDocumentation.document("products/save",
+                        PayloadDocumentation
+                                .requestFields(
+                                        PayloadDocumentation
+                                                .fieldWithPath("code")
+                                                .description("El identificador del producto"),
+                                        PayloadDocumentation
+                                                .fieldWithPath("name")
+                                                .description("El nombre del producto"),
+                                        PayloadDocumentation
+                                                .fieldWithPath("description")
+                                                .description("La descripcion del producto"),
+                                        PayloadDocumentation
+                                                .fieldWithPath("price")
+                                                .description("El precio del producto")
+                                )));
+
 /*
  Valida que el producto fue insertado exitosamente
         mockMvc
